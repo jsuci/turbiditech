b5472199b6c03452349ccfa8afb9b83735189f1a
jsuci <jsuci.jsuci@gmail.com>
2022-10-17 23:41:08 +0800

    add device, components and turbidity records model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Device
+from .models import CustomUser, Device, Component, TurbidityRecord
 
 admin.site.register(CustomUser)
-# admin.site.register(Device)
+admin.site.register(Device)
+admin.site.register(Component)
+admin.site.register(TurbidityRecord)
--- core/models.py
+++ core/models.py
@@ -65,7 +65,7 @@ class CustomUser(AbstractBaseUser):
 
 
     def __str__(self):
-        return self.email
+        return "%s %s" % (self.first_name, self.last_name)
 
     def has_perm(self, perm, obj=None):
         return True
@@ -77,21 +77,57 @@ class CustomUser(AbstractBaseUser):
     def is_staff(self):
         return self.is_admin
 
-class Device:
+
+class Device(models.Model):
     class DeviceStatus(models.TextChoices):
         ONLINE = 'online', 'online'
         OFFLINE = 'offline', 'offline'
 
-    device_name     = models.CharField(verbose_name='Device Name', max_length=120)
+    device_name     = models.CharField(verbose_name='Device Name', max_length=120, unique=True)
     location        = models.CharField(verbose_name='Location', max_length=120)
     install_date    = models.DateField(verbose_name='Install Date')
-    device_status   = models.CharField(max_length=7, choices=DeviceStatus.choices, default=DeviceStatus.ONLINE)
+    device_status   = models.CharField(verbose_name='Device Status', max_length=7, choices=DeviceStatus.choices, default=DeviceStatus.ONLINE)
+    managed_by      = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Managed By")
+
+    def __str__(self):
+        return self.device_name
 
     class Meta:
         verbose_name = 'device'
 
-class Component:
-    pass
 
-class TurbidityRecords:
-    pass
+class Component(models.Model):
+    component_name      = models.CharField(verbose_name='Component Name', max_length=120, unique=True)
+    device_link         = models.ForeignKey(Device, on_delete=models.CASCADE, verbose_name="Device Link")
+    install_date        = models.DateField(verbose_name='Install Date')
+    installed_by        = models.CharField(verbose_name='Installed By', max_length=120)
+
+    def __str__(self):
+        return f"{self.device_link}__{self.component_name}"
+
+    class Meta:
+        verbose_name = 'component'
+
+
+class TurbidityRecord(models.Model):
+
+    class VavleControl(models.TextChoices):
+        ON = 'on', 'on'
+        OFF = 'off', 'off'
+
+    class WaterStatus(models.TextChoices):
+        CLEAN = 'clean', 'clean'
+        DIRTY = 'dirty', 'dirty'
+
+    record_device       = models.ForeignKey(Device, on_delete=models.CASCADE, verbose_name="Record Device")
+    record_date         = models.DateField(verbose_name='Record Date', auto_now_add=True)
+    record_time         = models.TimeField(auto_now=False, auto_now_add=True, verbose_name='Record Time')
+    record_image        = models.CharField(verbose_name='Record Image', max_length=120, unique=True)
+    valve_status        = models.CharField(verbose_name='Vavle Status', max_length=3, choices=VavleControl.choices)
+    water_status        = models.CharField(verbose_name='Water Status', max_length=5, choices=WaterStatus.choices)
+
+    def __str__(self):
+        return f"{self.record_device}__{self.record_date}__{self.record_time}__{self.water_status}"
+
+    class Meta:
+        verbose_name = 'Turbidity Record'
