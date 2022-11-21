5b0eacf58487b7a776a162697e31f00172205e58
jsuci <jsuci.jsuci@gmail.com>
2022-11-21 23:05:37 +0800

    add new field to device record serialzier

--- core/serializers.py
+++ core/serializers.py
@@ -9,13 +9,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 class DeviceRecordSerializer(serializers.ModelSerializer):
+
+    device_name = serializers.SerializerMethodField('get_device_name')
+
     class Meta:
         model = TurbidityRecord
         fields = [
             'valve_status', 'water_status', 'record_image',
-            'record_device', 'details', 'created_on'
+            'record_device', 'details', 'created_on', 'device_name', 'id'
         ]
-        
+
+    def get_device_name(self, obj):
+        return obj.record_device.device_name
+
 
 class AllRecordSerializer(serializers.ModelSerializer):
     records = DeviceRecordSerializer(many=True)
